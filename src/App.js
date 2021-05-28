390d108778c58e99835240199d0eebcd8af5d0e1
Shoman-28 <northstar9928@gmail.com>
2021-05-28 22:30:03 +0600

    updat App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Login from "./Login";
 import { auth} from "./firebase"
 
 function App() {
+
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
 
@@ -37,18 +38,6 @@ function App() {
     <div className="App">
       {user ? <FastChatting/> : <Login/>}
       
-
-import React from "react";
-import { useSelector } from "react-redux";
-import "./App.css";
-import FastChatting from "./FastChatting";
-import { SelectUser } from "./features/UserSlice";
-function App() {
-  const user = useSelector(SelectUser);
-  return (
-    <div className="app">
-      {user ? <FastChatting></FastChatting> : <h2>You need to login</h2>}
-
     </div>
   );
 }
