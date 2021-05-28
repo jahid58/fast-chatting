07d5857cd88d37d60a54551a1bdfae67c70abf51
Jahid Hasan <jahidjjaa@gmail.com>
2021-05-28 21:22:20 +0600

    sidebar design

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,9 +1,31 @@
 import { Avatar } from "@material-ui/core";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import "./SidebarChat.css";
 const SidebarChat = () => {
+  const dispatch = useDispatch();
+  const [chatInfo, setChatInfo] = useState([]);
+
+  useEffect(() => {
+    db.collection("chats")
+      .doc(id)
+      .collection("messages")
+      .onSnapshot((snapshot) =>
+        setChatInfo(snapshot.docs.map((doc) => doc.data()))
+      );
+  }, [id]);
   return (
-    <div className="sidebarChat">
+    <div
+      onClick={() => {
+        dispatch(
+          setChat({
+            chatId: id,
+            chatName: chatName,
+          })
+        );
+      }}
+      className="sidebarChat"
+    >
       <Avatar></Avatar>
       <div className="sidebarChat__info">
         <h3>Channel Name</h3>
