d3f7a7cea9b3d28b3be9f89473b57a36f6a57144
Arnob Chakma <arnobchakma05@gmail.com>
2021-05-30 09:34:14 -0600

    my part

--- src/Chat.js
+++ src/Chat.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from "react";
-import "./Chat.css";
-import MicNoneIcon from "@material-ui/icons/MicNone";
-import Message from "./Message";
-import { useSelector } from "react-redux";
-import { SelectChatName, SelectChatId, chatSlice } from "./features/chatSlice";
-import db from "./firebase";
-import firebase from "firebase";
-import { SelectUser } from "./features/userSlice";
+import { IconButton } from '@material-ui/core';
+import { MicNone } from '@material-ui/icons';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { SelectUser } from "./features/UserSlice";
+import './Chat.css';
+import { SelectChatName, SelectChatId } from './features/ChatSlice';
+import db from './firebase';
+import MessageChat from './MessageChat';
+import firebase from 'firebase';
 import FlipMove from "react-flip-move";
-import { IconButton } from "@material-ui/core";
 
 const Chat = () => {
   const user = useSelector(SelectUser);
@@ -34,50 +34,58 @@ const Chat = () => {
     }
   }, [chatId]);
 
-  const sendMessage = (e) => {
-    e.preventDefault();
+    const sendMessage = (e) => {
+        e.preventDefault();
 
-    db.collection("chats").doc(chatId).collection("messages").add({
-      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      messages: input,
-      uid: user.uid,
-      photo: user.photo,
-      email: user.email,
-      displayName: user.displayName,
-    });
+        db.collection("chats")
+        .doc(chatId)
+        .collection("messages")
+        .add ({
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            message: input,
+            uid: user.uid,
+            photo: user.photo,
+            email: user.email,
+            displayName: user.displayName,
+        });
 
-    setInput("");
-  };
-  return (
-    <div className="chat">
-      <div className="chat__header">
-        <h4>
-          To: <span className="chat__name">{chatName}</span>
-        </h4>
-        <strong>Details</strong>
-      </div>
+        setInput("")
+    }
+    return (
+        <div className="chat">
+            <div className="chat__header">
+                <h4>To: <span className="chat__name">{chatName}</span> </h4>
+                <strong>Conversation</strong>
+            </div>
 
-      <div className="chat__messages">
-        {messages.map(({ id, data }) => (
-          <Message key={id} contents={data} />
-        ))}
-      </div>
-      <div className="chat__input">
-        <form>
-          <input
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            placeholder="FastChatting"
-            type="text"
-          />
-          <button onClick={sendMessage}>Send Message</button>
-        </form>
-        <IconButton>
-          <MicNoneIcon className="chat__mic" />
-        </IconButton>
-      </div>
-    </div>
-  );
-};
+            <div className="chat__messages">
+
+                <FlipMove>
+                    {
+                        messages.map(({id, data}) => (
+                            <MessageChat key={id} contents={data} />
+                        ))
+                    }
+                </FlipMove>
+                
+            </div>
+
+            <div className="chat__input">
+                <form action="">
+                    <input 
+                        value={input} 
+                        onChange={(e) => setInput(e.target.value)}
+                        type="text" 
+                        placeholder="Type a message" 
+                    />
+                    <button onClick={sendMessage}>Send Message</button>
+                </form>
+                <IconButton>
+                    <MicNone className="chat__mic"/>
+                </IconButton>
+            </div>
+        </div>
+    )
+}
 
 export default Chat;
