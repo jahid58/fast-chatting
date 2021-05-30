cf40cd33dd89d1a614d1cbc8060c2566e07cbeac
Jahid Hasan <jahidjjaa@gmail.com>
2021-05-30 19:49:06 +0600

    last commit

---
+++ src/Message.js
@@ -0,0 +1,24 @@
+import React, { forwardRef } from "react";
+import { Avatar } from "@material-ui/core";
+import { photoAlbumRounded } from "@material-ui/icons";
+import { useSelector } from "react-redux";
+
+import "./Message.css";
+import photo from "./image/person.png";
+import { SelectUser } from "./features/userSlice";
+
+const Message = ({
+  id,
+  contents: { timestamp, displayName, email, messages, photo, uid },
+}) => {
+  const user = useSelector(SelectUser);
+  return (
+    <div className={`massage ${user.email === email && "message__sender"}`}>
+      <Avatar className="message__photo" src={photo} />
+      <p>{messages}</p>
+      <small>{new Date(timestamp?.toDate()).toLocaleString()} </small>
+    </div>
+  );
+};
+
+export default Message;
